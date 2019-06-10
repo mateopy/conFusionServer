efb9243f2d0d0c1824aef104298c44bbbac1b665
Mateo Nuñez <mateo@web.com.py>
2019-06-10 08:33:35 -0400

    Assignment 2

---
+++ models/leaders.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+require('mongoose-currency').loadType(mongoose);
+
+var leadersSchema = new Schema({
+    name:  {
+        type: String,
+        required: true
+    },
+    image:  {
+        type: String,
+        required: true
+    },
+    designation:  {
+        type: String,
+        required: true
+    },
+    abbr:  {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    featured: {
+        type: Boolean,
+        default:false      
+    },
+}, {
+    timestamps: true
+});
+
+var Leaders = mongoose.model('Leader', leadersSchema);
+
+module.exports = Leaders;
